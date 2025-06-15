bf6fe7b1b3a1459c2daf6ca1cfe4f00bb7b0298f
snakehunterr <https://github.com/snakehunterr>
2025-06-15 12:10:32 +0300

    updated Makefile

--- Makefile
+++ Makefile
@@ -1,6 +1,2 @@
 build:
-	git describe --tags --abbrev=0 > args/version
-	go build -o fs .
-install:
-	git describe --tags --abbrev=0 > args/version
-	go build -o fs . && sudo mv ./fs /usr/bin/fs
+	go build -ldflags="-X main.version=$(shell git describe --tags --abbrev=0)" -o fs .
--- main.go
+++ main.go
@@ -1,22 +1,39 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"os"
+//
+// 	"github.com/snakehunterr/fsearch/args"
+// 	"github.com/snakehunterr/fsearch/walk"
+// )
+//
+// func main() {
+// 	args, err := args.Argparse()
+// 	if err != nil {
+// 		fmt.Fprintln(os.Stderr, err)
+// 		os.Exit(1)
+// 	}
+//
+// 	if err := walk.Walk(args); err != nil {
+// 		fmt.Fprintln(os.Stderr, err)
+// 		os.Exit(1)
+// 	}
+// }
+
 package main
 
 import (
 	"fmt"
-	"os"
-
-	"github.com/snakehunterr/fsearch/args"
-	"github.com/snakehunterr/fsearch/walk"
+	"runtime/debug"
 )
 
 func main() {
-	args, err := args.Argparse()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		fmt.Println("no version")
+		return
 	}
 
-	if err := walk.Walk(args); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
+	fmt.Println(info)
 }
